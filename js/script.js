f97849a504bc00aa898c40a4dd1458ac9f2717b8
Pablo Carvalho <pablo.luan.carvalho@gmail.com>
2025-03-27 20:06:22 +0000

    Upgrade

--- js/script.js
+++ js/script.js
@@ -56,3 +56,38 @@ document.addEventListener('DOMContentLoaded', function() {
   players.forEach(player => player.classList.remove('ativo'));
   players[0].classList.add('ativo');
 });
+ 
+document.querySelectorAll('.btn, button').forEach(button => {
+  button.addEventListener('click', () => {
+    const audio = new Audio('sounds/click.mp3');
+    audio.volume = 0.3;
+    audio.play();
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const texto = "Algomon presents: Guitar Rabbit";
+  const elemento = document.getElementById("typing");
+  let i = 0;
+  let apagando = false;
+
+  function digitar() {
+      if (!apagando && i < texto.length) {
+          elemento.innerHTML += texto.charAt(i);
+          i++;
+          setTimeout(digitar, 40);
+      } else if (apagando && i > 0) {
+          elemento.innerHTML = texto.substring(0, i - 1);
+          i--;
+          setTimeout(digitar, 50);
+      } else {
+          apagando = !apagando;
+          setTimeout(digitar, 3000);
+      }
+  }
+
+  digitar();
+});
+
+
